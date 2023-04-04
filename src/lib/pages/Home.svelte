db0f7956bb6b8ef00e7a8a4446e8d40c8bba7c01
GrungeElFz <elf0114_@hotmail.com>
2023-04-04 00:08:20 -0500

    Modified:   import Button from '../Button.svelte' (src/lib/pages/Home.svelte)             Assigned a let variable `showButton = false`
                Added an <input type="checkbox" bind:checked={showButton} />
                Added 3 <Button> components
                Wrapped the third <Button> with an {#if showButton} block

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,2 +1,13 @@
 <script>
+	import Button from '../Button.svelte';
+	let showButton = false;
 </script>
+
+<input type="checkbox" bind:checked={showButton} />
+
+<Button>Button</Button>
+<Button>Button</Button>
+
+{#if showButton}
+	<Button>Button</Button>
+{/if}
