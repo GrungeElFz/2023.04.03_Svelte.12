6ab13938fff8ea1829ac8ad0e16d1785a081d379
GrungeElFz <elf0114_@hotmail.com>
2023-04-03 19:11:02 -0500

    Modified:   Cleared template (src/lib/pages/Home.svelte)

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,20 +1,2 @@
 <script>
-	import { Stage, Layer, Rect } from '../konva';
-
-	let showCanvas = true;
 </script>
-
-<input type="checkbox" bind:checked={showCanvas} />
-
-{#if showCanvas}
-	<Stage width={300} height={400}>
-		<Layer>
-			<Rect x={20} y={20} fill="MediumSlateBlue" width={200} height={100} />
-			<Rect x={50} y={50} fill="DeepSkyBlue" width={100} height={100} />
-		</Layer>
-
-		<Layer>
-			<Rect x={80} y={80} fill="gold" width={100} height={100} />
-		</Layer>
-	</Stage>
-{/if}
