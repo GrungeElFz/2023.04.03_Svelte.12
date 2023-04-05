33fce8b5de90303444f03341897cf8d1fdc2374a
GrungeElFz <elf0114_@hotmail.com>
2023-04-04 23:44:56 -0500

    Modified:   Cleared the entire file (src/lib/pages/Home.svelte)
    Modified:   Removed {totalButtons} (src/lib/Button.svelte)

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,23 +1,2 @@
 <script>
-	import Button, { getTotalButtons } from '../Button.svelte';
-	let showButton = false;
-	let total;
 </script>
-
-<input type="checkbox" bind:checked={showButton} />
-
-<Button>Button</Button>
-<Button>Button</Button>
-
-{#if showButton}
-	<Button>Button</Button>
-{/if}
-
-{total}
-<button
-	on:click={() => {
-		total = getTotalButtons();
-	}}
->
-	Update buttons count
-</button>
