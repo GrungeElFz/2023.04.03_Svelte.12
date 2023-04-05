8f4db3a348625feffda9d70820b69d771a5054fa
GrungeElFz <elf0114_@hotmail.com>
2023-04-05 16:00:42 -0500

    Modified:   Assigned a let variable `paused = true` (src/lib/VideoPlayer.svelte)             Assigned a let variable `currentTime = 0`             Assigned a let variable `volume = 0`
                In a <video> element
                - Added `bind=paused`
                - Added `bind=volume`
                - Added `bind=currentTime`
    
                Added a Play/Pause <button> element
    
                Added a {currentTime} display
                Added a {volume} display

--- src/lib/VideoPlayer.svelte
+++ src/lib/VideoPlayer.svelte
@@ -1,5 +1,20 @@
 <script>
-  export let src = undefined;
+	export let src = undefined;
+
+	let paused = true;
+	let currentTime = 0;
+	let volume = 0;
 </script>
 
-<video {src} controls muted/>
+<video {src} controls muted bind:paused bind:volume bind:currentTime />
+
+<button
+	on:click={() => {
+		paused = !paused;
+	}}
+>
+	{paused ? 'Play' : 'Pause'}
+</button>
+
+<p>{currentTime}</p>
+<p>{volume}</p>
